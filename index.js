9df01374d201b9ea2ccb788d37f94f31dbc3aa8e
jozzzz128 <jozzzz128@gmail.com>
2020-03-04 16:40:05 -0600

    Hola mundo con parametros

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("Hello world");
+app.get("/:name", (req, res, next) =>{
+    res.status(200);
+    res.send("Hola "+req.params.name);
 });
 
 app.listen(3000, () =>{
-    console.log("lol");
+    console.log("Server is running...");
 });

45530548a232bdcc3b3fcaf8116e89525cf3174c
jozzzz128 <jozzzz128@gmail.com>
2020-03-04 16:56:53 -0600

    Se preparo el servidor para el desarrollo

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res, next) =>{
+    res.send("Bienvenido!!");
+});
+
 app.get("/:name", (req, res, next) =>{
     res.status(200);
     res.send("Hola "+req.params.name);
 });
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running...");
 });

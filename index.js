6a898318b59000d2f0e986968e66756633cad40d
jozzzz128 <jozzzz128@gmail.com>
2020-03-23 21:52:15 -0600

    Se incorporo RegEx para validaciones y se agrego una ruta para buscar por nombres

--- index.js
+++ index.js
@@ -7,14 +7,31 @@ app.get("/", (req, res, next) =>{
     res.send("Bienvenido al Pokedex");
 });
 
-app.get("/pokemon", (req, res, next) =>{
+app.get("/pokemon/all", (req, res, next) =>{
     res.status(200);
     res.send(pokemon);
 });
 
-app.get("/pokemon/:id", (req, res, next) => {
-    res.status(200);
-    res.send(pokemon[req.params.id - 1]);
+app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
+    const id = req.params.id - 1;
+    if(id >= 0 && id <= 150) {
+        res.status(200);
+        return res.send(pokemon[req.params.id - 1]);
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
+app.get("/pokemon/:name", (req, res, next) => {
+    const name = req.params.name;
+    for(let i = 0; i < pokemon.length; i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
 });
 
 app.listen(process.env.PORT || 3000, ()=>{

340c929e5bd1409925d036a9e9241ee02318adfc
jozzzz128 <jozzzz128@gmail.com>
2020-03-23 21:32:47 -0600

    Se incorporo pokedex.json y se agregaron 2 rutas para acceder a el

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
 
 app.get("/", (req, res, next) =>{
-    res.send("Bienvenido!!");
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
 });
 
-app.get("/:name", (req, res, next) =>{
+app.get("/pokemon", (req, res, next) =>{
     res.status(200);
-    res.send("Hola "+req.params.name);
+    res.send(pokemon);
+});
+
+app.get("/pokemon/:id", (req, res, next) => {
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
 });
 
 app.listen(process.env.PORT || 3000, ()=>{

476ad528e0b161fb884dee88171c0a79e67ba351
jozzzz128 <jozzzz128@gmail.com>
2020-03-23 22:16:20 -0600

    Se incorporaron operadores ternarios y se hicieron validaciones de texto

--- index.js
+++ index.js
@@ -2,36 +2,42 @@ const express = require('express');
 const app = express();
 const { pokemon } = require('./pokedex.json');
 
+/* VERBOS
+    GET - Obtener recursos
+    POST - almacenar/crear recursos
+    PATCH - modificar una parte de un recurso
+    PUT - modificar un recurso
+    DELETE - borrar un recurso
+*/
+
+/* OPERADOR TERNARIO
+    condicion ? valor si verdadero : valor si falso
+*/
+
 app.get("/", (req, res, next) =>{
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.get("/pokemon/all", (req, res, next) =>{
-    res.status(200);
-    res.send(pokemon);
+app.get("/pokemon/", (req, res, next) =>{
+    return res.status(200).send(pokemon);
 });
 
 app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
     const id = req.params.id - 1;
-    if(id >= 0 && id <= 150) {
-        res.status(200);
-        return res.send(pokemon[req.params.id - 1]);
-    }
-    res.status(404);
-    res.send("Pokemon no encontrado");
+    (id >= 0 && id <= 150) ? 
+        res.status(200).send(pokemon[req.params.id - 1]) : 
+        res.status(404).send("Pokemon no encontrado");
 });
 
-app.get("/pokemon/:name", (req, res, next) => {
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
     const name = req.params.name;
-    for(let i = 0; i < pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Pokemon no encontrado");
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    });
+
+    (pk.length > 0) ? 
+        res.status(200).send(pk) : 
+        res.status(404).send("Pokemon no encontrado");
 });
 
 app.listen(process.env.PORT || 3000, ()=>{

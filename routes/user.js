8483f279d8f648098bdd768f4df9de8c696388d1
jozzzz128 <jozzzz128@gmail.com>
2020-05-09 23:21:46 -0500

    Se incorporo jwt y se agrego una ruta a user.js

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -15,8 +16,27 @@ user.post("/", async (req, res, next) => {
     return res.status(500).json({ code: 500, message: "Campos incompletos" });
 });
 
+user.post("/login", async (req, res, next) => {
+    const { user_mail, user_password } = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+    const rows = await db.query(query);
+    
+    if (user_mail && user_password){
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({ code: 200, message: token });
+        }
+        return res.status(401).json({ code: 401, message: "Usuario y/o usuario incorrectos!!" });
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+    
+});
+
 user.get("/", async (req, res, next) => {
-    const query = "SELECT * FROM user";
+    const query = "SELECT * FROM user;";
     const rows = await db.query(query);
 
     return res.status(200).json({ code: 200, message: rows});
